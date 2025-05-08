a1cbbb83c2cf54d6a4c653814143600d8fa14fbb
raffs <raffs.cardoso15@gmail.com>
2025-05-07 21:27:05 -0300

    using loops and Scanner

---
+++ src/jogoAdivinhacao.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class jogoAdivinhacao {
+    public static void main(String[] args) {
+        int numAleatorio = new Random().nextInt(100);
+        Scanner leitura = new Scanner(System.in);
+        int tentativas = 0;
+        int numUser = 0;
+
+        while(tentativas < 5){
+            System.out.println("Qual o numero secreto?");
+            numUser = leitura.nextInt();
+
+
+            if(numUser == numAleatorio){
+                System.out.println("Numero correto!");
+                break;
+            } else if (numUser < numAleatorio) {
+                System.out.println("O numero digitado e menor que o numero aleatorio!");
+            }else
+            System.out.println("O numero digitado e maior que o numero secreto!");
+
+            if (numUser != numAleatorio && tentativas == 5){
+                System.out.println("Voce falhou! O numero secreto era: " + numAleatorio);
+            }
+            tentativas++;
+        }
+    }
+}
