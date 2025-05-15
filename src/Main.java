ab1273376f106379866fe9ff577df4e601780c20
raffs <raffs.cardoso15@gmail.com>
2025-05-15 11:01:26 -0300

    more exercises

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import br.com.raffs.listExercises2.Carro;
+import br.com.raffs.listExercises3.Aluno;
 import br.com.raffs.listExercises3.IdadePessoa;
 import br.com.raffs.listExercises3.Produto;
 
 public class Main {
     public static void main(String[] args) {
-        Produto produto = new Produto();
-        produto.setPreco(200.00);
-        produto.aplicarDesconto(10);
+        Aluno aluno = new Aluno();
+        aluno.setNota1(10);
+        aluno.setNota2(8);
+        aluno.setNota3(3);
+        aluno.calculaMedia();
     }
 }
